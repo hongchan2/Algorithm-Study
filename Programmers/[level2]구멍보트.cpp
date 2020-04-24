d33fc89b2b1b888fea2bb63c7b619d8d16f335ee
hongchan2 <ipeter@naver.com>
2020-04-24 12:40:33 +0900

    구멍보트 (Greedy)

--- Programmers/[level2]구멍보트.cpp
+++ Programmers/[level2]구멍보트.cpp
@@ -8,8 +8,6 @@ int solution(vector<int> people, int limit) {
     int answer = 0;
     sort(people.begin(), people.end());
 
-    int i = 0;
-    int j = people.size() - 1;
     for(int i = 0, j = people.size() - 1; i <= j; j--){
         if(people[i] + people[j] <= limit) i++;
 
