5d6b58818c7674707e0d7992dd1a446987c1e736
hongchan2 <ipeter@naver.com>
2020-05-09 12:50:02 +0900

    호텔 방 배정 (카카오 기출) (Union-Find)

--- Programmers/[level4]호텔 방 배정_2.cpp
+++ Programmers/[level4]호텔 방 배정_2.cpp
@@ -1,10 +1,3 @@
-/*
-    visit_list 타입은 long long 형인데, int 타입으로 받아서 
-    효율성 3가지 테스트 케이스에서 TLE 받음
-
-    -> 입력의 범위가 클 때는 항상 타입에 신경을 쓰기!
-*/
-
 #include <vector>
 #include <unordered_map>
 
@@ -12,7 +5,6 @@ using namespace std;
 
 vector<long long> solution(long long k, vector<long long> room_number) {
     vector<long long> answer;
-    // current, parent
     unordered_map<long long, long long> m;
     
     for(long long num : room_number){
@@ -20,27 +12,28 @@ vector<long long> solution(long long k, vector<long long> room_number) {
         auto iter = m.find(num);
         
         if(iter == m.end()){
-            m.insert(make_pair(num, num + 1));
+            m.insert(make_pair(num, 0));
             answer.push_back(num);
         }
         else{
             visit_list.push_back(num);
+            long long i = num + 1;
 
-            long long i = iter->second;
+            if(iter->second != 0) i += iter->second;
 
-            while(true){
+            for(; i <= k; i++){
                 iter = m.find(i);
 
                 if(iter != m.end()){
                     visit_list.push_back(i);
-                    i = iter->second;
+                    if(iter->second != 0) i += iter->second;
 
                     continue;
                 }
                 
-                m.insert(make_pair(i, i + 1));
+                m.insert(make_pair(i, 0));
                 for(long long value : visit_list){
-                    m[value] = i;
+                    m[value] = i - value;
                 }
                 answer.push_back(i);
                 break;
